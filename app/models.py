32707ecb214561d2cd763aa1137984606e8a37e9
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-09-20 19:54:40 +0000

    admin + contacts

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    phone = models.IntegerField()
+    info = models.CharField(max_length=150)
+    gender = models.CharField(max_length=50, choices=(
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ))
+    image = models.ImageField(upload_to='images/', blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
