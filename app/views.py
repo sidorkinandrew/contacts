76514c98b80e9de040753ed1c724ad4720a470ba
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-09-21 23:09:36 +0000

    search page/search term

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Contact
 
@@ -30,7 +30,12 @@ class ContactDetailView(DetailView):
     context_object_name = 'contact'
 
 def search(request):
-    context = {
-        
-    }
-    return render(request, 'search.html', context)
+#    context = {'search_term': "",}
+    if request.GET:
+        search_term = request.GET['search_term']
+        context = {
+            'search_term': search_term,
+        }
+        return render(request, 'search.html', context)
+    else:
+        return redirect('home')
